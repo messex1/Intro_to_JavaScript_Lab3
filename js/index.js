b01ce6d287ad5fcafa9830f00958d8b6c952ec2f
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-03-13 20:54:45 -0600

    misc update

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ const showResourcesButton = document.getElementById('show-resources');
 // Select all div elements with class 'list-group-item', as well as other elements that need the I-Beam hover effect removed
 const elementsWithHoverEffect = document.querySelectorAll('.list-group-item, .p, .h4, .strong, .h1');
 
-
 // Event handler function for the button that removes the class d-none from the div element and makes links bold
 function showResources() {
     // Remove the 'd-none' class from the div element
@@ -23,9 +22,6 @@ function showResources() {
 // Add an event listener on the button that listens to the click event and call showResources function
 showResourcesButton.addEventListener('click', showResources);
 
-// Select all div elements with class 'list-group-item'
-const divs = document.querySelectorAll('.list-group-item');
-
 // Event handler function for mouseover event to make the text within the hovered div bold
 function mouseoverHandler(e) {
     // Add the 'fw-bold' class to the text within the hovered div
@@ -39,19 +35,7 @@ function mouseoutHandler(e) {
 }
 
 // Add event listeners to each div element to handle mouseover and mouseout events independently
-divs.forEach(div => {
-    div.addEventListener('mouseover', mouseoverHandler);
-    div.addEventListener('mouseout', mouseoutHandler);
-});
-
-
-//removes I-Beam hover effect on div elements
-// Loop through each div and set user-select property to none
-divs.forEach(div => {
-    div.style.userSelect = 'none';
-});
-
-// Loop through each element and set user-select property to none
 elementsWithHoverEffect.forEach(element => {
-    element.style.userSelect = 'none';
+    element.addEventListener('mouseover', mouseoverHandler);
+    element.addEventListener('mouseout', mouseoutHandler);
 });
