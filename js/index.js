b900826aac178ca9d791986df008d4fce9e43cea
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-03-13 20:49:32 -0600

    Hover effect mod
    attempted to remove hover effect on .h4, h1, .p, .strong but couldn't.... perhaps its a linked stylesheet in my html code

--- js/index.js
+++ js/index.js
@@ -1,33 +1,57 @@
-//reference variable to  div element with id resources
+// Reference variable to the div element with the id of resources
 const resourcesDiv = document.getElementById('resources');
 
-//reference variable to button with id show-resources
+// Reference variable to button with id show-resources
 const showResourcesButton = document.getElementById('show-resources');
 
+// Select all div elements with class 'list-group-item', as well as other elements that need the I-Beam hover effect removed
+const elementsWithHoverEffect = document.querySelectorAll('.list-group-item, .p, .h4, .strong, .h1');
 
 
-//event listener on button that listen to  click event
-showResourcesButton.addEventListener('click', function() {
-
-// Event handler function for button element id
-// removes class d-none from div element
+// Event handler function for the button that removes the class d-none from the div element and makes links bold
+function showResources() {
+    // Remove the 'd-none' class from the div element
     resourcesDiv.classList.remove('d-none');
+
+    // Select all links inside the div and make them bold by adding 'fw-bold' class
+    const links = resourcesDiv.querySelectorAll('a');
+    links.forEach(link => {
+        link.classList.add('fw-bold');
+    });
+}
+
+// Add an event listener on the button that listens to the click event and call showResources function
+showResourcesButton.addEventListener('click', showResources);
+
+// Select all div elements with class 'list-group-item'
+const divs = document.querySelectorAll('.list-group-item');
+
+// Event handler function for mouseover event to make the text within the hovered div bold
+function mouseoverHandler(e) {
+    // Add the 'fw-bold' class to the text within the hovered div
+    e.currentTarget.classList.add('fw-bold');
+}
+
+// Event handler function for mouseout event to remove bold text
+function mouseoutHandler(e) {
+    // Remove the 'fw-bold' class from the text within the div
+    e.currentTarget.classList.remove('fw-bold');
+}
+
+// Add event listeners to each div element to handle mouseover and mouseout events independently
+divs.forEach(div => {
+    div.addEventListener('mouseover', mouseoverHandler);
+    div.addEventListener('mouseout', mouseoutHandler);
 });
 
 
-//e is event object.
+//removes I-Beam hover effect on div elements
+// Loop through each div and set user-select property to none
+divs.forEach(div => {
+    div.style.userSelect = 'none';
+});
 
-function eventHandlerFunction(e) {
-    // Check event type is mouseover, add 'fw-bold' class to element
-    if (e.type === 'mouseover') {
-        e.target.classList.add('fw-bold');
-    } else if (e.type === 'mouseout') {
-        // If event type mouseout, remove 'fw-bold' class from element
-        e.target.classList.remove('fw-bold');
-    }
-}
-
-//event listener to div element with id resources
-
-resourcesDiv.addEventListener('mouseover', eventHandlerFunction);
-resourcesDiv.addEventListener('mouseout', eventHandlerFunction);
+// Loop through each element and set user-select property to none
+elementsWithHoverEffect.forEach(element => {
+    element.style.userSelect = 'none';
+});
